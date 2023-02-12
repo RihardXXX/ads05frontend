7996f9ad6d59a2e7343d92f3aa78093de2200bd2
RihardXXX <yusupa.akaeva@yandex.ru>
2023-02-12 11:30:22 +0300

    завершил работу над версткой футера

--- src/components/layot/footer/Footer.tsx
+++ src/components/layot/footer/Footer.tsx
@@ -1,11 +1,63 @@
 import React from 'react';
 import styles from './footer.module.scss';
 import classNames from 'classnames';
+import { NavLink } from 'react-router-dom';
+import { ReactComponent as Home } from 'assets/icons/home.svg';
+import { ReactComponent as Favorite } from 'assets/icons/favorite.svg';
+import { ReactComponent as My } from 'assets/icons/my.svg';
+import { ReactComponent as Add } from 'assets/icons/add.svg';
 
 const Footer = (): JSX.Element => {
-    const classes = classNames([[styles.footer]]);
+    const footerClasses = classNames([[styles.footer]]);
 
-    return <footer className={classes}>footer</footer>;
+    return (
+        <footer className={footerClasses}>
+            <nav>
+                <ul className={styles.list}>
+                    <li className={styles.item}>
+                        <NavLink
+                            to="/"
+                            className={({ isActive }) =>
+                                isActive ? styles._active : undefined
+                            }
+                        >
+                            <Home />
+                        </NavLink>
+                    </li>
+                    <li className={styles.item}>
+                        <NavLink
+                            to="favorites"
+                            className={({ isActive }) =>
+                                isActive ? styles._active : undefined
+                            }
+                        >
+                            <Favorite />
+                        </NavLink>
+                    </li>
+                    <li className={styles.item}>
+                        <NavLink
+                            to="my"
+                            className={({ isActive }) =>
+                                isActive ? styles._active : undefined
+                            }
+                        >
+                            <My />
+                        </NavLink>
+                    </li>
+                    <li className={styles.item}>
+                        <NavLink
+                            to="create"
+                            className={({ isActive }) =>
+                                isActive ? styles._active : undefined
+                            }
+                        >
+                            <Add />
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </footer>
+    );
 };
 
 export default Footer;
--- src/components/layot/header/Header.tsx
+++ src/components/layot/header/Header.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import styles from './header.module.scss';
+import { useLocation } from 'react-router-dom';
 import classNames from 'classnames';
 
 const Header = (): JSX.Element => {
     const classes = classNames([[styles.header]]);
+
+    const location = useLocation();
+
+    console.log('location: ', location.pathname);
 
     return <header className={classes}>header</header>;
 };
