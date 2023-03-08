a701a273a2253a923cc08027d9478d5bd2f1452b
RihardXXX <yusupa.akaeva@yandex.ru>
2023-03-08 10:41:37 +0300

    завершение верстки деталки объявления

--- src/pages/detailAdvert/DetailAdvert.tsx
+++ src/pages/detailAdvert/DetailAdvert.tsx
@@ -3,12 +3,30 @@ import styles from './detailAdvert.module.scss';
 import classNames from 'classnames';
 import { ReactComponent as Watch } from 'assets/icons/watch.svg';
 import Button from 'components/common/button';
+import { ReactComponent as Heart } from 'assets/icons/favorite.svg';
 
 const DetailAdvert = () => {
     const classes = classNames([[styles.h2], { [styles.h2]: true, xxx: true }]);
 
+    const isFavorite = false;
+
+    const isFavoriteIcon = classNames([
+        [styles.heart],
+        { [styles._isFavorite]: isFavorite },
+    ]);
+    const isFavoriteCount = classNames([
+        [styles.count],
+        { [styles._isFavorite]: isFavorite },
+    ]);
+
     return (
         <div className={styles.detailPage}>
+            <div className={styles.authorSection}>
+                <div className={styles.icon}>
+                    <img src="" alt="" />
+                </div>
+                <div className={styles.author}>author</div>
+            </div>
             <div className={styles.wrapperHeader}>
                 <div className={styles.watch}>
                     <Watch />
@@ -30,6 +48,36 @@ const DetailAdvert = () => {
                 <Button name="теги" type="outline" color="black" />
                 <Button name="теги" type="outline" color="black" />
                 <Button name="теги" type="outline" color="black" />
+            </div>
+            <div className={styles.contactSection}>
+                <h5 className={styles.title}>contact:</h5>
+                <p className={styles.description}>
+                    Lorem ipsum dolor sit amet consectetur. Velit et sodales
+                    sociis sapien quis egestas. Mauris commodo mi faucibus sit
+                    eget in non. Eu enim molestie hendrerit quisque malesuada
+                    nisl sit.
+                </p>
+            </div>
+            <div
+                className={styles.wrapFavorite}
+                onClick={() => console.log(112)}
+            >
+                <Heart className={isFavoriteIcon} />
+                <div className={isFavoriteCount}>0</div>
+            </div>
+
+            <div className={styles.commentSection}>
+                <h5 className={styles.title}>comments:</h5>
+                {[1, 2, 3, 4, 5, 6].map((index) => {
+                    return (
+                        <article key={index} className={styles.card}>
+                            <h6 className={styles.author}>author</h6>
+                            <p className={styles.comment}>
+                                content jfhgkehdkdshkfs
+                            </p>
+                        </article>
+                    );
+                })}
             </div>
         </div>
     );
